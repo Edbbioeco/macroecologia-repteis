ef83f1ead3748b8e838dd585b9ce29a66ebda740
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-13 11:44:04 -0300

    Script para organizar as comunidades geradas pelo levantamentoF

--- comunidades_levantamento.R
+++ comunidades_levantamento.R
@@ -14,13 +14,54 @@ library(writexl)
 
 ## Cidades ----
 
+### Importando ----
+
+cidades <- geobr::read_municipality(year = 2019)
+
+### Visualizando ----
+
+cidades
+
+ggplot() +
+  geom_sf(data = cidades)
+
 ## Grade -----
+
+### Importando ----
+
+grade <- sf::st_read("grade_fom.shp")
+
+### Visualizando ----
+
+grade
+
+ggplot() +
+  geom_sf(data = grade)
 
 ## Registros -----
 
+### Importando ----
+
+lev <- readxl::read_xlsx("dados copilados.xlsx")
+
+### Visualizando ----
+
+lev
+
+lev |> dplyr::glimpse()
+
+### Tratando ----
+
+lev_trat <- lev |> 
+  tidyr::pivot_longer(cols = dplyr::where(is.numeric),
+                      values_to = "presence",
+                      names_to = "city")
+
+lev_trat
+
 # Georreferenciamento dos registros ----
 
-## Coordenadas das espécies ----
+## Coordenadas das cidades ----
 
 ## Transformando os pontos em shapefile ----
 
