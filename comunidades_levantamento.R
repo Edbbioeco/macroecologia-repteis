93c14b3d217cf345a12a24d5cf4ef0e6e914999b
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-13 11:37:52 -0300

    Script para organizar as comunidades geradas pelo levantamentoF

---
+++ comunidades_levantamento.R
@@ -0,0 +1,34 @@
+# Pacote ----
+
+library(geobr)
+
+library(tidyverse)
+
+library(sf)
+
+library(readxl)
+
+library(writexl)
+
+# Dados ----
+
+## Cidades ----
+
+## Grade -----
+
+## Registros -----
+
+# Georreferenciamento dos registros ----
+
+## Coordenadas das espécies ----
+
+## Transformando os pontos em shapefile ----
+
+## Intersecção com a FOM ----
+
+# Matriz de composição ----
+
+## Montando a matriz ----
+
+## Exportando ----
+
