5521a7c97b383ce2e4e0e7009dbdb54b4e6170da
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-13 09:58:06 -0300

    Script para organizar as comunidades geradas plos registrso do GBIF

---
+++ comunidades_gbif.R
@@ -0,0 +1,25 @@
+# Pacotes ----
+
+# Dados ----
+
+## Importando ----
+
+## Visualizando ----
+
+## Unindo ----
+
+# Recortar para a FOM ----
+
+## Transformando em shapefile ----
+
+## Intersectando para a FOM ----
+
+# Matriz de composição ----
+
+## Lista de espécies ----
+
+## tratando as espécies ----
+
+## Montando a matriz de composição ----
+
+## Exportando ----
