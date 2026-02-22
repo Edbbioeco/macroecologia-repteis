85afd27e9ab1e8ea28d6e521dece39dc9053c7e0
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-22 15:54:13 -0300

    Script para organizar as comunidades geradas pelos registros do SiBBr

--- comunidades_sibbr.R
+++ comunidades_sibbr.R
@@ -74,8 +74,56 @@ sibbr_sf_fom |>
 
 ## tratando as espécies ----
 
-# Matriz de composição ----
+sibbr_sf_fom <- sibbr_sf_fom |> 
+  dplyr::mutate(Species = dplyr::case_match(
+    Species,
+    "Tomodon dorsatum"             ~ "Tomodon dorsatus",
+    "Tupinambis merianae"          ~ "Salvator marianae",
+    "Mabuya frenata"               ~ "Notomabuya frenata",
+    "Anisiolepis grilli"           ~ "Urostrophus grilli",
+    "Mabuya dorsivittata"          ~ "Aspronema dorsivittatum",
+    "Sibynomorphus neuwiedi"       ~ "Dipsas neuwiedi",
+    "Liotyphlops beui"             ~ "Liotyphlops ternetzii",
+    "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
+    "Pantodactylus schreibersii"   ~ "Cercosaura schreibersii",
+    "Bothrops neuwiedi diorus"     ~ "Bothrops neuwiedi",
+    "Mastigodryas bifossatus"      ~ "Palusophis bifossatus",
+    "Liophis miliaris"             ~ "Erythrolamprus miliaris",
+    "Sibynomorphus mikanii"        ~ "Dipsas mikanii",
+    "Liophis jaegeri"              ~ "Erythrolamprus jaegeri",
+    "Phalotris iheringii"          ~ "Phalotris lemniscatus",
+    "Thamnodynastes hypoconia"     ~ "Dryophylax hypoconia",
+    "Thamnodynastes strigatus"     ~ "Mesotes strigatus",
+    "Atractus taeniatus"           ~ "Atractus paraguayensis",
+    "Crotalus durissus terrificus" ~ "Crotalus durissus",
+    "Echinanthera affinis"         ~ "Dibernardia affinis",
+    "Bothrops newwiedi"            ~ "Bothrops neuwiedi",
+    c("Bothrops trigemina", 
+      "Anolis philopunctatus", 
+      "Lygophis lineatus", 
+      "Tupinambis teguixin", 
+      "Bothrops neuwiedi paranaensis", 
+      "Heterodactylus imbricatus", 
+      "Dipsas indica", 
+      "Boiruna maculata", 
+      "Clelia plúmbea", 
+      "Xenodon biligonigerus")      ~ NA_character_,
+    .default = Species
+  ))
+
+sibbr_sf_fom
 
 ## Montando a matriz de composição ----
 
+sibbr_registros <- sibbr_sf_fom |> 
+  sf::st_join(grade) |> 
+  as.data.frame() |> 
+  dplyr::mutate(Especies = Species,
+                Presence =  1) |> 
+  dplyr::select(ID, Especies, Presence) 
+
+sibbr_registros
+
 ## Exportando ----
+
+sibbr_registros |> writexl::write_xlsx("registros_sibbr.xslx")
