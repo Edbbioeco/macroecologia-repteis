1e5ab20c4a931df0638c3339543a4d5b3f3af103
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-22 16:16:21 -0300

    Script para organizar as comunidades gerais

--- montando_matriz_composição_taxonomica.R
+++ montando_matriz_composição_taxonomica.R
@@ -1,5 +1,42 @@
 # Pacotes ----
 
+library(tidyverse)
+
+library(readxl)
+
+library(writxl)
+
 # Dados ----
+
+## Importando ----
+
+importar_comunidades <- function(registro){
+  
+  comunidade <- readxl::read_xlsx(paste0("registros_",
+                                         registro,
+                                         ".xlsx"))
+  
+  assign(paste0("comunidade_", registro),
+         comunidade,
+         envir = globalenv())
+  
+}
+
+registro <- c("gbif", "specieslink", "sibbr", "levantamento")
+
+registro
+
+purrr::map(registro, importar_comunidades)
+
+## Visualizando ----
+
+ls(pattern = "comunidade_") |> 
+  mget(envir = globalenv())
+
+## Unindo ----
+
+ls(pattern = "comunidade_") |> 
+  mget(envir = globalenv()) |> 
+  dplyr::bind_rows()
 
 # Matriz de composição taxonomica ----
