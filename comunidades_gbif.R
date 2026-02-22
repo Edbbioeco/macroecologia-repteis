36fca42717e4df9b6fc94c257092eaa70a58ae69
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-22 15:40:40 -0300

    Script para organizar as comunidades geradas pelos registros do GBIF

--- comunidades_gbif.R
+++ comunidades_gbif.R
@@ -104,7 +104,44 @@ gbif_sf_fom |>
 
 ## tratando as espécies ----
 
-# Matriz de composição ----
+gbif_sf_fom <- gbif_sf_fom |> 
+  dplyr::mutate(species = dplyr::case_match(
+    species,
+    "Tomodon dorsatum"             ~ "Tomodon dorsatus",
+    "Tupinambis merianae"          ~ "Salvator marianae",
+    "Mabuya frenata"               ~ "Notomabuya frenata",
+    "Anisiolepis grilli"           ~ "Urostrophus grilli",
+    "Mabuya dorsivittata"          ~ "Aspronema dorsivittatum",
+    "Sibynomorphus neuwiedi"       ~ "Dipsas neuwiedi",
+    "Liotyphlops beui"             ~ "Liotyphlops ternetzii",
+    "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
+    "Pantodactylus schreibersii"   ~ "Cercosaura schreibersii",
+    "Bothrops neuwiedi diorus"     ~ "Bothrops neuwiedi",
+    "Mastigodryas bifossatus"      ~ "Palusophis bifossatus",
+    "Liophis miliaris"             ~ "Erythrolamprus miliaris",
+    "Sibynomorphus mikanii"        ~ "Dipsas mikanii",
+    "Liophis jaegeri"              ~ "Erythrolamprus jaegeri",
+    "Phalotris iheringii"          ~ "Phalotris lemniscatus",
+    "Thamnodynastes hypoconia"     ~ "Dryophylax hypoconia",
+    "Thamnodynastes strigatus"     ~ "Mesotes strigatus",
+    "Atractus taeniatus"           ~ "Atractus paraguayensis",
+    "Crotalus durissus terrificus" ~ "Crotalus durissus",
+    "Echinanthera affinis"         ~ "Dibernardia affinis",
+    "Bothrops newwiedi"            ~ "Bothrops neuwiedi",
+    c("Bothrops trigemina", 
+      "Anolis philopunctatus", 
+      "Lygophis lineatus", 
+      "Tupinambis teguixin", 
+      "Bothrops neuwiedi paranaensis", 
+      "Heterodactylus imbricatus", 
+      "Dipsas indica", 
+      "Boiruna maculata", 
+      "Clelia plúmbea", 
+      "Xenodon biligonigerus")      ~ NA_character_,
+    .default = species
+  ))
+
+gbif_sf_fom
 
 ## Montando a matriz de composição ----
 
