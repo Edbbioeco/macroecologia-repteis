c50827fe541fff233bf5570b112a1b32915dcf8f
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-08 10:51:13 -0300

    Visualizar o shapefile daas Florestas Ombrófilas Densas

--- mapa_fom.R
+++ mapa_fom.R
@@ -26,3 +26,11 @@ ggplot() +
 ## Importar ----
 
 fom <- sf::st_read("fom.shp")
+
+## Visualizar ----
+
+fom
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = fom, color = "forestgreen")
