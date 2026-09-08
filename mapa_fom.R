786189d20347f7b68df68e6a198169f2394cd6d1
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-08 11:10:48 -0300

    Criar o mapa principal

--- mapa_fom.R
+++ mapa_fom.R
@@ -50,3 +50,42 @@ ggplot() +
   geom_sf(data = ma, color = "green", fill = "green") +
   geom_sf(data = fom, color = "darkgreen", fill = "darkgreen") +
   geom_sf(data = br, color = "black", fill = "transparent")
+
+# Mapa ----
+
+## Mapa principal ----
+
+mapa_principal <- ggplot() +
+  geom_sf(data = br, 
+          aes(color = "Brazil", fill = "Brazil"),
+          linewidth = 1) +
+  geom_sf(data = ma, 
+          aes(color = "Atlantic Forest", fill = "Atlantic Forest")) +
+  geom_sf(data = fom, 
+          aes(color = "FOM", fill = "FOM")) +
+  geom_sf(data = br, color = "black", fill = "transparent", 
+          linewidth = 1) +
+  coord_sf(xlim = c(-54.04717, -44.25722),
+           ylim = c(-30.36529, -22.56086)) +
+  scale_color_manual(values = c("Brazil" = "black",
+                                "Atlantic Forest" = "green",
+                                "FOM" = "darkgreen"),
+                     breaks = c("Brazil", 
+                                "Atlantic Forest",
+                                "FOM")) +
+  scale_fill_manual(values = c("Brazil" = "white",
+                               "Atlantic Forest" = "green",
+                               "FOM" = "darkgreen"),
+                    breaks = c("Brazil", 
+                               "Atlantic Forest",
+                               "FOM")) +
+  labs(color = NULL,
+       fill = NULL) +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 20),
+        legend.text = element_text(color = "black", size = 20),
+        legend.position = "bottom",
+        panel.border = element_rect(color = "black", linewidth = 2)) +
+  ggview::canvas(height = 10, width = 12)
+
+mapa_principal
