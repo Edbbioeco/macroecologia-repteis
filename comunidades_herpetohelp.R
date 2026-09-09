9c6eff7046a91cea217d57a712f270c62dae4d76
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-09 13:24:07 -0300

    Recortar os registros para a área das Florestas Ombrófilas Mistas

--- comunidades_herpetohelp.R
+++ comunidades_herpetohelp.R
@@ -35,3 +35,28 @@ herpetohelp <- readxl::read_xlsx("herptohelp.xlsx",,
 herpetohelp
 
 herpetohelp |> dplyr::glimpse()
+
+# Recortar para a FOM ----
+
+## Transformando em shapefile ----
+
+inaturalist_sf <- herpetohelp |>
+  dplyr::filter(Grupo == "Répteis") |>  
+  dplyr::filter(!`Longitude no mapa (SIRGAS 2000)` |> is.na() &
+                  !`Latitude no mapa (SIRGAS 2000)` |> is.na() &
+                  !Espécie |> is.na() &
+                  !Espécie |> 
+                  stringr::str_detect(" sp$| sp.$| sp,$| sp | aff| cf,") &
+                  !Espécie |>
+                  stringr::str_count(stringr::boundary("word")) == 1) |> 
+  dplyr::mutate(`Latitude no mapa (SIRGAS 2000)` = `Latitude no mapa (SIRGAS 2000)` |> as.numeric(),
+                Espécie = Espécie |> 
+                  stringr::str_replace("^(\\S+\\s+\\S+)\\s+\\S+(.*)", 
+                                       "\\1\\2")) |> 
+  sf::st_as_sf(coords = c("Longitude no mapa (SIRGAS 2000)", "Latitude no mapa (SIRGAS 2000)"),
+               crs = grade |> sf::st_crs())
+
+inaturalist_sf
+
+ggplot() +
+  geom_sf(data = inaturalist_sf)
